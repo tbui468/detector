11fbeb5c748acff555bb276efa069e22e733c46a
Thomas <thomasthienbui@gmail.com>
2021-06-08 08:47:57 +0700

    Added Profile and requirements

--- src/app.py
+++ src/app.py
@@ -2,39 +2,48 @@ from flask import Flask, request, render_template_string
 from PIL import Image
 import os, shutil
 import time
-import torch
-import detect
+#import torch
+#import detect
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    print(request.files)
-    out_path = ''
-    if request.files:
-        img = request.files['image']
-        #img = Image.open(image)
-        result = detect.predict(img)
-        folder = 'static'
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-            os.unlink(file_path)
+#get basic flask app running locally (DONE!)
+#get basic flask app running on heroku
+#get detector working with flask locally
+#get detector working on heroku
 
-        ts = time.time()
-        out_path = 'static/output' + str(ts) + '.png'
-        result.save(out_path)
+def test():
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        print(request.files)
+        out_path = ''
+        if request.files:
+            img = request.files['image']
+            #img = Image.open(image)
+            result = detect.predict(img)
+            folder = 'static'
+            for filename in os.listdir(folder):
+                file_path = os.path.join(folder, filename)
+                os.unlink(file_path)
 
-    #render template is reading from same directory as __init__.py, 
-    #but img.save is reading from src directory
-    return render_template_string(
-            '''
-            <form method="POST" enctype="multipart/form-data">
-                <input type="file" name="image"/>
-                <button type="submit">Submit</button>
-            </form>
-            <img src="{{output}}" alt="test output">
-            <h1>Nope</h1>
-            ''', output=out_path)
+            ts = time.time()
+            out_path = 'static/output' + str(ts) + '.png'
+            result.save(out_path)
 
+        #render template is reading from same directory as __init__.py, 
+        #but img.save is reading from src directory
+        return render_template_string(
+                '''
+                <form method="POST" enctype="multipart/form-data">
+                    <input type="file" name="image"/>
+                    <button type="submit">Submit</button>
+                </form>
+                <img src="{{output}}" alt="test output">
+                <h1>Nope</h1>
+                ''', output=out_path)
+
+@app.route('/')
+def hello():
+    return 'hello world'
 
 
     if __name__ == '__main__':
